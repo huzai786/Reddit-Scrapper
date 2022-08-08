10858f99f688452ca11642a942250dd3788cab40
muhammad huzaifa <muhammadhuzaifagamer123@gmail.com>
2022-08-08 16:04:47 +0500

    Pytest config

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
 import json
-import time
-import argparse
+
 try:
+    import click
     from requests_html import HTMLSession
     from fake_useragent import UserAgent
     from packages.utils import (
@@ -10,29 +10,31 @@ try:
         find_posts_links,
         get_posts_data,
     )
-    
 except ModuleNotFoundError as e:
     print(e)
     print('Please download the required modules from requirements.txt')
     sys.exit()
     
+UA = UserAgent()
+SESSION = HTMLSession()
 
-
-
-
-
-def main(ua, session, tag):
-    try: 
-        divs = get_tag_data(tag, session)
+@click.command()
+@click.option('--tag', prompt='Please enter a valid sub-reddit tag name!', help='Enter sub-reddit tag name!')
+def main(tag):
+    """Store last 10 posts for the tags subreddit"""
+    click.echo('starting the script!')
+    global UA 
+    global SESSION
+    try:
+        divs = get_tag_data(tag, SESSION)
         links = find_posts_links(divs)
-        posts = get_posts_data(links, session, ua)
+        posts = get_posts_data(links, SESSION, UA)
         data = []
-        for p in posts:
-            data.append(p)
+        for post in posts:
+            data.append(post)
 
-        with open(f"{tag}.json", "w", encoding="utf-8") as f:
-            json.dumps(data, f)
-            
+        with open(f"{tag_name}.json", "w", encoding="utf-8") as file_name:
+            json.dumps(data, file_name)  
             
     except KeyboardInterrupt:
         print('Exiting!')
@@ -42,7 +44,4 @@ def main(ua, session, tag):
 
 
 if __name__ == "__main__":
-    ua = UserAgent()
-    session = HTMLSession()
-    tag = "Python"
-    main(ua, session, tag)
+    main()
--- packages/utils.py
+++ packages/utils.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup, Tag, NavigableString
 from requests_html import HTMLSession
 from fake_useragent import UserAgent
-from typing import Generator
+from typing import Generator, Type, Tuple, Any
+from collections import namedtuple
 import traceback
 import requests
 import datetime
@@ -13,12 +14,10 @@ def get_tag_data(reddit_tag: str, session) -> Tag:
     """Render the page and returns the selected div containing relevant data"""
 
     try:
-        r = session.get(
-            f"https://www.reddit.com/r/{reddit_tag}/new/"
-        )
-        print("Rendering...") 
-        r.html.render(sleep=2, scrolldown=1, wait=3, timeout=30) 
-        soup = BeautifulSoup(r.html.html, "lxml") 
+        r = session.get(f"https://www.reddit.com/r/{reddit_tag}/new/")
+        print("Rendering...")
+        r.html.render(sleep=2, scrolldown=1, wait=3, timeout=30)
+        soup = BeautifulSoup(r.html.html, "lxml")
         divs = soup.select("div > div#AppRouter-main-content")[0]
         return divs
 
@@ -29,36 +28,32 @@ def get_tag_data(reddit_tag: str, session) -> Tag:
 
 
 def _extract_link(tag: Tag) -> str:
-    """Helper Function"""
-    
     try:
         atag = tag.find("a", {"data-click-id": "body"})
         if atag is not None:
-            link = atag["href"]
-            return f"https://www.reddit.com{link}"
+            link = "https://www.reddit.com" + atag["href"]
+            return link
 
     except Exception:
         print("Link not found!")
-        return None
+        return ""
 
 
 def _handle_time(time_msg: str) -> str:
-    """Helper Function"""
-    
     time_list = time_msg.split()
-    time_sec = time_list[0]
+    time_sec = float(time_list[0])
     if time_list[1] == "hour":
         time_sec = time_sec * 3600
     if time_list[1] == "minutes":
         time_sec = time_sec * 60
-    time = datetime.datetime(seconds=time_sec)
+    time = datetime.datetime.now() - datetime.timedelta(seconds=time_sec)
     time_str = time.strftime("%y:%M:%d %I:%M")
     return time_str
 
 
 def find_posts_links(Bs4Tag: Tag) -> Tag:
     """Finds all the 'Posts' Links on the page and returns then as list"""
-    
+
     post_links = []
     if Bs4Tag is not None:
         divs = Bs4Tag.find_all("div", {"data-testid": "post-container"})
@@ -66,13 +61,38 @@ def find_posts_links(Bs4Tag: Tag) -> Tag:
             span = div.find("span", string="promoted")
             if span is None:
                 link = _extract_link(div)
-                if link is not None:
+                if link != "":
                     post_links.append(link)
-    print(f'{len(post_links)} Links found!')
+    print(f"{len(post_links)} Links found!")
     return post_links
 
 
-def get_posts_data(post_links: list, session: HTMLSession, ua: UserAgent) -> Generator:
+def _data_handle(values_tags: Type[Tuple[Any, ...]]) -> dict:
+    post_data = {}
+    description = ""
+    if desc != []:
+        for d in desc:
+            description += d.get_text()
+            description += "\n"
+    if time is not None:
+        time_str = _handle_time(time.get_text())
+    if user is not None:
+        user_str = user.get_text()
+    if title is not None:
+        title_str = title.get_text()
+    if description == "":
+        description = "description not found"
+
+    post_data["author"] = user_str
+    post_data["posted_at"] = time_str
+    post_data["title"] = title_str
+    post_data["description"] = description
+    print(f"Post Author: {user_str}")
+    pass
+
+
+def get_posts_data(
+    post_links: list, session: HTMLSession, ua: UserAgent) -> Type[Generator]:
     """Returns a generator for the Posts data when iterated on each link"""
 
     for link in post_links[:10]:
@@ -83,49 +103,33 @@ def get_posts_data(post_links: list, session: HTMLSession, ua: UserAgent) -> Gen
                 headers=headers,
             )
             r.html.render(sleep=1, scrolldown=1, wait=3, timeout=30)
-            post_data = {}
             soup = BeautifulSoup(r.html.html, "lxml")
             content = soup.find("div", {"data-test-id": "post-content"})
-            time = content.find(
+            posted_on = content.find(
                 "span",
                 {"data-click-id": "timestamp", "data-testid": "post_timestamp"},
             )
-            user = content.select_one('div > a[data-testid="post_author_link"]')
+            author = content.select_one('div > a[data-testid="post_author_link"]')
             title = content.select_one(
                 'div[data-adclicklocation="title"] > div > div > h1'
             )
             desc = content.select('div[data-click-id="text"] > div')[0]
             desc = desc.find_all("p")
-            description = ""
-            if desc != []:
-                for d in desc:
-                    description += d.get_text()
-                    description += "\n"
-            if time is not None:
-                time_str = _handle_time(time.get_text())
-            if user is not None:
-                user_str = user.get_text()
-            if title is not None:
-                title_str = title.get_text()
-            if description == "":
-                description = "description not found"
+            postDataObjects = namedtuple('post_data_object', ['author', 'posted_on', 'title', 'description'])
+            post_data_variables = postDataObjects(author=author, posted_on=posted_on, title=title, description=desc)
+            post_data_dict = _data_handle(post_data_variables)
 
-            post_data["author"] = user_str
-            post_data["posted_at"] = time_str
-            post_data["title"] = title_str
-            post_data["description"] = description
-            print(f'Post Author: {user_str}')
-            yield post_data
+            yield post_data_dict
 
-        except requests.RequestException:
-            print('Got RequestException!')
-            traceback.print_exc()
+        except IndexError:
+            print("Value Not Found!")
+            return {}
 
         except requests.TooManyRedirects:
             print("To many request!")
             time.sleep(5)
             pass
-        
-        except IndexError:
-            print("Value Not Found!")
-            pass
+
+        except requests.RequestException:
+            traceback.print_exc()
+            sys.exit()
